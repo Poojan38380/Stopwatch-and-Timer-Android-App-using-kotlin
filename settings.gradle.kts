5536cacc62d24292787bf1f31af789852c95c683
Poojan <poojangoyani@gmail.com>
2023-10-17 18:03:24 +0530

    first commit, ui created. Backend and functionlity left

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Stopwatch_Timer_Android_app_using_Kotlin"
+include(":app")
+ 
